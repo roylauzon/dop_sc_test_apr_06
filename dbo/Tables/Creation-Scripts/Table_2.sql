84f7fbf542888f5f344fb3dbf6e971ae7f175970
roylauzon <roylauzon>
2024-06-17 16:07:38 +0300

    adding table 5, alter table 2; TaskID: table15

--- dbo/Tables/Creation-Scripts/Table_2.sql
+++ dbo/Tables/Creation-Scripts/Table_2.sql
@@ -5,6 +5,7 @@ GO
 CREATE TABLE [dbo].[Table_2](
 	[id2] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
 	[id2_1] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
-	[id2_2] [int] NULL
+	[id2_2] [int] NULL,
+	[id2_3] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
