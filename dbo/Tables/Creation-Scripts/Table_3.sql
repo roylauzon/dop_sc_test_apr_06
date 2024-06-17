4eceb84b98c74403a0786284697c9f9040ee237c
roylauzon <roylauzon>
2024-06-17 16:08:43 +0300

    alter table 3; TaskID: table15

--- dbo/Tables/Creation-Scripts/Table_3.sql
+++ dbo/Tables/Creation-Scripts/Table_3.sql
@@ -4,6 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_3](
 	[id3] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
-	[id_3_2] [int] NULL
+	[id_3_2] [int] NULL,
+	[id_3_3] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
