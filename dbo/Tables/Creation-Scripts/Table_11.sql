e741b22aa456f82f94e2598d1096d3e0cac1cf87
roylauzon <roylauzon>
2024-06-17 15:58:51 +0300

    adding  column 3 to table 11; TaskID: table11

--- dbo/Tables/Creation-Scripts/Table_11.sql
+++ dbo/Tables/Creation-Scripts/Table_11.sql
@@ -4,6 +4,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_11](
 	[id_11_1] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
-	[id_11_2] [int] NULL
+	[id_11_2] [int] NULL,
+	[id_11_3] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
