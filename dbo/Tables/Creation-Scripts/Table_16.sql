93efe7b62019436defbaf96746dc75fe45bec587
roylauzon <roylauzon>
2024-06-25 13:05:00 +0300

    add column to table 16; TaskID: t16_change

--- dbo/Tables/Creation-Scripts/Table_16.sql
+++ dbo/Tables/Creation-Scripts/Table_16.sql
@@ -3,6 +3,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Table_16](
-	[t16_col1] [nchar](10) COLLATE Latin1_General_CI_AS NULL
+	[t16_col1] [nchar](10) COLLATE Latin1_General_CI_AS NULL,
+	[t16_col2] [nchar](10) COLLATE Latin1_General_CI_AS NULL
 )
 GO
